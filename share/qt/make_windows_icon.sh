77af1045266bf2bd1c038bfe9dd9626deb9ae61c
root <root@explorer.unitcurrency.ccom>
2016-12-31 12:18:52 +0700

    Remove old version

--- share/qt/make_windows_icon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/UniversalCurrency.ico
-
-convert ../../src/qt/res/icons/UniversalCurrency-16.png ../../src/qt/res/icons/UniversalCurrency-32.png ../../src/qt/res/icons/UniversalCurrency-48.png ${ICON_DST}
